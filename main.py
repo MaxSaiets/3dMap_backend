634ffe293a5f71b9cb487c25027cdbafecfcaaf4
Max Saiets <saietsmax@gmail.com>
2026-02-01 12:01:16 +0200

    Allow CORS from all origins

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ app = FastAPI(title="3D Map Generator API", version="1.0.0")
 # CORS налаштування для інтеграції з frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
